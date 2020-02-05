f33581efd462fdb8db1ee0e15f1152ca45bfbf7d
MaximAlesich <maka-mogilev@mail.ru>
2020-02-05 13:16:52 +0300

    Вчера работал с другого компа. Дошел до 2 метода...и завис. Сегодня доделал, делал вчера все с начала.

---
+++ Task1 continue/Task1 continue/Helpik.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_continue
+{
+    public static class Helpik
+    {
+        public static string DataInput(string textToShowInConsole)
+        {
+            Console.WriteLine(textToShowInConsole);
+            string TheLetter = Console.ReadLine();
+            return TheLetter;
+        }
+      
+        public static void SwapData(string first, string second)
+        {
+            string temp = first;
+            first = second;
+            second = temp;
+        }
+    }
+}
+
+
+
---
+++ Task1 continue/Task1 continue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string TheLetterA;
+            string TheLetterB;
+
+            TheLetterA = Helpik.DataInput("Введите первое значение: ");
+            TheLetterB = Helpik.DataInput("Введите второе значение: ");
+
+            Console.WriteLine("Поменять значения местами?");
+            Helpik.SwapData(TheLetterA, TheLetterB);
+            Console.ReadKey();
+
+            Console.WriteLine($"Вот Ваши буквы, стоящие наоборот: {TheLetterB}, {TheLetterA}");
+            Console.ReadKey();
+        }
+    }
+}
